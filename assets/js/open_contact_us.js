6cafc1dfe63194961913a380ffeaf14ed66fbb72
changyungchun <changyungchun@users.noreply.github.com>
2025-05-16 02:02:48 +0000

    deploy: 66f25f474fac046c4ffc6c1bfa5a45c085a0c0ce

--- assets/js/open_contact_us.js
+++ assets/js/open_contact_us.js
@@ -1,11 +1,16 @@
 // open_contact_us.js
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll("nav a");
-  
-    links.forEach(link => {
-      if (link.href === "https://www.tejwin.com/en/contact/") {
-        link.setAttribute("target", "_blank");
-        link.setAttribute("rel", "noopener noreferrer");
-      }
-    });
+  const links = document.querySelectorAll("nav a");
+
+  links.forEach(link => {
+    const targetUrls = [
+      "https://www.tejwin.com/en/contact/",
+      "https://www.tejwin.com/en/insight-category/data-analysis/"
+    ];
+    
+    if (targetUrls.includes(link.href)) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+    }
   });
+});
